972509fdc03da3dba4f68c714748fcc6c8df31dd
A.S <alexander.soulimov@gmail.com>
2011-12-06 15:47:49 +0400

    Traversing the tree

--- Compiler/compiler.py
+++ Compiler/compiler.py
@@ -1,6 +1,82 @@
 # coding: utf-8
+import logging
 
 __author__ = "Alexander Soulimov (alexander.soulimov@gmail.com)"
 __copyright__ = "Copyright (c) 2011 A.Soulimov"
 __license__ = "Python"
-  
+from main import set_logging
+from lexer import get_tokens
+from bas_parser import runParser
+
+local_variables = dict()
+input_parameters = list()
+
+def get_classification(tree):
+	"""
+
+	@param tree:
+	@return:
+	"""
+	# если уже в листе
+	if type(tree) != dict:
+		print "TERMINAL: " + str(tree)
+		return tree
+	else:
+		print tree
+		attr = tree.keys()[0]
+		print tree[attr]
+		results = list()
+		if type(tree[attr]) is list:
+			for subtree in tree[attr]:
+				results.append(get_classification(subtree))
+			if attr == "instr":
+				if results[0] == "LET":
+					if not local_variables.has_key(results[1][0]):
+						local_variables[results[1][0]] = list()
+						res_val = None
+						try:
+							res_val = int(results[3][0])
+						except Exception:
+							res_index = results[3][0][0][1]
+							if not res_index:
+								res_index = 0
+							else:
+								res_index = res_index[0]
+							res_val = local_variables[results[3][0][0][0]][res_index]
+
+						local_variables[results[1][0]].append(res_val)
+				elif results[0] == "GET":
+					if not local_variables.has_key(results[1][0]):
+						local_variables[results[1][0]] = list()
+
+						res_ind = int(local_variables[results[1][1][1]][0]) - 1
+						local_variables[results[1][0]].append(input_parameters[res_ind])
+				elif results[0] == "JUMP":
+					pass
+			elif attr == "R3":
+				pass
+			elif attr == "expr":
+				pass
+
+
+			return results
+		else:
+			return tree[attr]
+
+
+
+def runCompiler(tree):
+	get_classification(tree)
+
+if __name__ == "__main__":
+	global input_parameters
+	set_logging(logging.DEBUG, logging.DEBUG, "./../log/")
+	code_file = open('./../Input/example1.txt', 'r')
+	code_lines = code_file.readlines()
+	code_file.close()
+	chain_lines, input_variables = get_tokens(code_lines)
+	input_parameters = map(lambda x: int(x), input_variables)
+	#chain_lines = [";", "asdasdasdasdqwe asd", "END"]
+	tree = runParser(chain_lines)
+	print tree
+	runCompiler(tree)
--- Compiler/lexer.py
+++ Compiler/lexer.py
@@ -15,19 +15,29 @@ def get_tokens(code):
 	"""
 
 	tokens = list()
+	input = False
+	input_values = list()
 	for code_line in code:
 		# prepare the code line
 		# assume that at least one blank or tab separates each token
 		code_line = code_line.replace('\t', ' ').replace('\n', '').replace('  ', ' ').strip()
 		if code_line == '':
 			continue
+		line_tokens = code_line.split(' ')
 
-		tokens.extend(code_line.split(' '))
+		if not input:
+			tokens.extend(line_tokens)
+		else:
+			input_values.append(line_tokens[0])
+		if 'END' in line_tokens:
+			input = True
 
-	return tokens
+	return tokens, input_values
 
 if __name__ == "__main__":
 	code_file = open('./../Input/example1.txt', 'r')
 	code_lines = code_file.readlines()
 	code_file.close()
-	print get_tokens(code_lines)
+	t, inp = get_tokens(code_lines)
+	print t
+	print inp
